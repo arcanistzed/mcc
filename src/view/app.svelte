e121b31b47bc19398a01c8f59406bcde126f7080
arcanist <arcanistzed@gmail.com>
2022-05-02 20:51:32 -0400

    feat: display rows in a table
    Signed-off-by: arcanist <arcanistzed@gmail.com>

--- src/view/app.svelte
+++ src/view/app.svelte
@@ -3,6 +3,9 @@
 
 <script>
 	import { ApplicationShell } from "@typhonjs-fvtt/runtime/svelte/component/core";
+	import SpreadsheetController from "../controller/SpreadsheetController.js";
+	let rows = [];
+	(async () => (rows = await SpreadsheetController.getRows()))();
 
 	export let elementRoot;
 </script>
@@ -11,7 +14,23 @@
 <!-- ApplicationShell exports `elementRoot` which is the outer application shell element -->
 <ApplicationShell bind:elementRoot>
 	<main>
-		
+		<table>
+			<thead><tr><th colspan="7">Module Compatibility</th></tr></thead>
+			<tbody>
+				{#each rows as row}
+					<tr>
+						<td>{row[0] ?? ""}</td>
+						<td>{row[1] ?? ""}</td>
+						<td>{row[2] ?? ""}</td>
+						<td>{row[3] ?? ""}</td>
+						<td>{row[4] ?? ""}</td>
+						<td>{row[5] ?? ""}</td>
+						<td>{row[6] ?? ""}</td>
+						<td>{row[7] ?? ""}</td>
+					</tr>
+				{/each}
+			</tbody>
+		</table>
 	</main>
 </ApplicationShell>
 
