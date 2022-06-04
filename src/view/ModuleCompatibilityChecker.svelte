dd590aae897654c41b472c81b8ff63d915c19739
arcanist <arcanistzed@gmail.com>
2022-06-04 16:44:27 -0400

    fix: overflow window header text https://gitlab.com/foundrynet/foundryvtt/-/issues/7220
    Signed-off-by: arcanist <arcanistzed@gmail.com>

--- src/view/ModuleCompatibilityChecker.svelte
+++ src/view/ModuleCompatibilityChecker.svelte
@@ -1,6 +1,7 @@
 <svelte:options accessors={true} />
 
 <script>
+	import { onMount } from "svelte";
 	import { ApplicationShell } from "@typhonjs-fvtt/runtime/svelte/component/core";
 	import SpreadsheetController from "../controller/SpreadsheetController.js";
 	import Loading from "./Loading.svelte";
@@ -21,6 +22,14 @@
 			errorMessage = error;
 			console.error(error);
 		});
+
+	// Fix window header text overflow (https://gitlab.com/foundrynet/foundryvtt/-/issues/7220)
+	onMount(() => {
+		const title = elementRoot.querySelector(".window-title");
+		title.style.textOverflow = "ellipsis";
+		title.style.overflow = "hidden";
+		title.style.whiteSpace = "nowrap";
+	});
 
 	export let elementRoot;
 </script>
