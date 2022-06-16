8995b0c34082df9b07b0197aa0dfff6b12110262
arcanist <arcanistzed@gmail.com>
2022-06-15 21:27:23 -0400

    feat: patreon button in module management window
    This partially reverts commit e087949386d8dc7a9c077d03d3846cf934c45646
    
    Signed-off-by: arcanist <arcanistzed@gmail.com>

--- src/init.js
+++ src/init.js
@@ -1,4 +1,13 @@
 import ModuleCompatibilityChecker from "./view/ModuleCompatibilityChecker.js";
+import PatreonButton from "./view/PatreonButton.svelte";
 
 Hooks.once("ready", () => new ModuleCompatibilityChecker().render(true, { focus: true }));
 globalThis.ModuleCompatibilityChecker = ModuleCompatibilityChecker;
+
+Hooks.on("renderModuleManagement", (app, [html]) => {
+    console.log(html);
+    new PatreonButton({
+		target: html.querySelector("[data-module-id='mcc'] .package-overview"),
+		anchor: html.querySelector("[data-module-id='mcc'] .tag"),
+	});
+});
---
+++ src/view/PatreonButton.svelte
@@ -0,0 +1,21 @@
+<span
+	class="tag patreon"
+	on:click={() => window.open("https://www.patreon.com/bePatron?u=15896855", "_blank")}
+>
+	<i class="fab fa-patreon" />
+	Patreon support
+</span>
+
+<style>
+	.tag.patreon {
+		background-color: hsl(357, 100%, 63%);
+		transition: all 300ms cubic-bezier(0.19, 1, 0.22, 1) 0s;
+		cursor: pointer;
+	}
+	.tag.patreon:active {
+		background-color: hsl(356, 100%, 58%);
+	}
+	.tag.patreon:hover {
+		background-color: hsl(356, 100%, 60%);
+	}
+</style>
