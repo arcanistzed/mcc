68c8a025dc7fdad39042c791ca283aa069d5dd4a
arcanist <arcanistzed@gmail.com>
2022-06-15 21:30:09 -0400

    feat: open with module management button
    Signed-off-by: arcanist <arcanistzed@gmail.com>

--- src/init.js
+++ src/init.js
@@ -1,13 +1,17 @@
 import ModuleCompatibilityChecker from "./view/ModuleCompatibilityChecker.js";
 import PatreonButton from "./view/PatreonButton.svelte";
+import ModuleManagementButton from "./view/ModuleManagementButton.svelte";
 
-Hooks.once("ready", () => new ModuleCompatibilityChecker().render(true, { focus: true }));
 globalThis.ModuleCompatibilityChecker = ModuleCompatibilityChecker;
 
-Hooks.on("renderModuleManagement", (app, [html]) => {
-    console.log(html);
-    new PatreonButton({
+Hooks.on("renderModuleManagement", async (_app, [html]) => {
+	new PatreonButton({
 		target: html.querySelector("[data-module-id='mcc'] .package-overview"),
 		anchor: html.querySelector("[data-module-id='mcc'] .tag"),
+	});
+
+	new ModuleManagementButton({
+		target: html.querySelector("footer"),
+		anchor: html.querySelector("[name='deactivate']"),
 	});
 });
---
+++ src/view/ModuleManagementButton.svelte
@@ -0,0 +1,8 @@
+<script>
+    import { localize } from "../utils.js";
+    import ModuleCompatibilityChecker from "./ModuleCompatibilityChecker.js";
+</script>
+
+<button on:click={() => new ModuleCompatibilityChecker().render(true, { focus: true })}>
+    { localize("button")}
+</button>
