de24fb718e6cc4632470c57812c89e53735757f5
arcanist <arcanistzed@gmail.com>
2022-07-08 00:52:58 -0400

    style: comments
    Signed-off-by: arcanist <arcanistzed@gmail.com>

--- src/init.js
+++ src/init.js
@@ -8,11 +8,13 @@ globalThis.ModuleCompatibilityChecker = ModuleCompatibilityChecker;
 Hooks.on("renderModuleManagement", (app, [html]) => {
 	applyModuleManagementColors(app, html);
 
+	// Add a Patreon button next to this module
 	new PatreonButton({
 		target: html.querySelector(":is([data-module-id='mcc'], [data-module-name='mcc']) .package-overview"),
 		anchor: html.querySelector(":is([data-module-id='mcc'], [data-module-name='mcc']) .tag"),
 	});
 
+	// Add a button to the bottom of the module management window
 	new ModuleManagementButton({
 		target: html.querySelector("footer"),
 		anchor: html.querySelector("[name='deactivate']"),
