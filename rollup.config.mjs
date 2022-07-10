bd298d00de9931a88863829cec8955450fe16de4
typhonrt <support@typhonjs.io>
2022-07-10 14:41:48 -0700

    Update TRL / remove Babel from build as no longer needed as Foundry core is on ES2022

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,4 +1,3 @@
-import { babel } from "@rollup/plugin-babel";
 import postcss from "rollup-plugin-postcss"; // Process Sass / CSS w/ PostCSS
 import resolve from "@rollup/plugin-node-resolve"; // This resolves NPM modules from node_modules.
 import svelte from "rollup-plugin-svelte";
@@ -57,19 +56,7 @@ export default () => {
 
 				postcss(postcssMain),
 
-				resolve(RESOLVE_CONFIG),
-
-				// Transpile only for production builds
-				PRODUCTION &&
-					babel({
-						babelHelpers: "bundled",
-						presets: [
-							[
-								"@babel/preset-env",
-								{ bugfixes: true, shippedProposals: true, targets: { esmodules: true } },
-							],
-						],
-					}),
+				resolve(RESOLVE_CONFIG)
 			],
 			onwarn(warning, warn) {
 				// Suppress warning from library code
