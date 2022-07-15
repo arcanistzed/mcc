e746413d5f8bb67d025878015f4951461c2184f4
typhonrt <support@typhonjs.io>
2022-07-14 21:37:26 -0700

    Update for latest TRL

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -19,7 +19,7 @@ const postcssMain = postcssConfig({
 
 const RESOLVE_CONFIG = {
 	browser: true,
-	dedupe: ['svelte', '@typhonjs-fvtt/runtime', '@typhonjs-fvtt/svelte-standard']
+	dedupe: ['svelte']
 };
 
 export default () => {
