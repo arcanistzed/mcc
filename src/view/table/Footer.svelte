d6d28bfa87e545951822c4f635396389840bed92
arcanist <arcanistzed@gmail.com>
2022-07-21 16:47:01 -0400

    fix: prevent small gap at bottom
    This reverts commit f3278e78fbeba9d3d31975552da23687de291851
    
    Signed-off-by: arcanist <arcanistzed@gmail.com>

--- src/view/table/Footer.svelte
+++ src/view/table/Footer.svelte
@@ -27,7 +27,7 @@
 <style>
 	footer {
 		position: sticky;
-        bottom: 0;
+		bottom: -0.5px; /* A small negative value prevents a 1px gap from sporadically showing */
 		background-color: #283048;
 		color: white;
 		text-align: center;
